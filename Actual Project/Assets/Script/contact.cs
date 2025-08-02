afaeaf8eb45b29dabf14b1a778b020472365b764
raudhah <raudhahhsulaiman@gmail.com>
2025-08-02 14:42:49 +0800

    display from database

---
+++ Actual Project/Assets/Script/contact.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class contact : MonoBehaviour
+{
+    public Text bandar;
+    public Text kb;
+
+    public string dbURL = "http://localhost/hackathon/contact.php";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(GetData());
+    }
+
+    public IEnumerator GetData()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(dbURL);
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            string json = www.downloadHandler.text;
+            Debug.Log("Json from php: " + json);
+
+            json = "{\"contacts\":" + json + "}";
+            Debug.Log("wrraped json: " + json);
+
+            ContactList contactList = JsonUtility.FromJson<ContactList>(json);
+
+            foreach (ContactData contact in contactList.contacts) {
+                if (contact.name.Contains("HQ"))
+                    bandar.text = "Address: " + contact.address + "\nPhone: " + contact.phone;
+                else if (contact.name.Contains("Kuala Belait"))
+                    kb.text = "Address: " + contact.address + "\nPhone: " + contact.phone;
+            }
+        }
+        else
+        {
+            Debug.LogError("Error: " + www.error);
+        }
+    }
+
+    [System.Serializable]
+    public class ContactData {
+        public string name, address, phone;
+    }
+
+    [System.Serializable]
+    public class ContactList {
+        public ContactData[] contacts;
+    }
+}

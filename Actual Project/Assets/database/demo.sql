213885b2668a1dbe1f689be190c74270fb3ffa63
waejax <qawiem.waezul@gmail.com>
2025-09-02 11:02:32 +0800

    demo sql

--- Actual Project/Assets/database/demo.sql
+++ Actual Project/Assets/database/demo.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 19, 2025 at 02:47 PM
+-- Generation Time: Sep 02, 2025 at 04:59 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,19 +37,24 @@ CREATE TABLE `demo` (
 --
 
 INSERT INTO `demo` (`id`, `line`) VALUES
-(1, 'Hello, Welcome to Awang Budiman: Leap Towards Goodness!'),
+(1, 'Hello, Welcome to (name)!'),
 (2, 'I am Awang Budiman'),
 (3, 'I will be your \'budi\' throughout this game'),
-(4, 'Use the left and right arrow to move, and space bar to jump'),
-(5, 'Now you know how to move along the screen!'),
-(6, 'You just encounter an info icon'),
-(7, 'Hit more icon to learn interesting info!'),
-(8, 'You just encounter an evidence'),
-(9, 'Collect as many evidence as you can'),
-(10, 'This will help you on the upcoming game'),
-(11, 'The number on the top left will show you the number of evidence you have collected'),
-(12, 'You are required to collect 2 evidence to move to the next stage'),
-(13, 'You are now ready to start playing!');
+(4, 'Press the right arrow key to move to the right'),
+(5, 'and press the left arrow key to move to the left'),
+(6, 'Press on the space bar to jump'),
+(7, 'Now you know how to move along the screen!'),
+(8, 'You just encounter an info icon'),
+(9, 'Hit more icon to learn interesting info!'),
+(10, 'You just encounter an evidence'),
+(11, 'Collect as many evidence as you can'),
+(12, 'This will help you on the upcoming game'),
+(13, 'The number on the top left will show you the number of evidence you have collected'),
+(14, 'You are required to collect a minimum of 2 evidence before you can move to the next stage'),
+(15, 'You are now ready to start playing!'),
+(16, 'Welcome to the first level.'),
+(17, 'Here in this level, we learn good values.'),
+(18, 'Hit the info icon to learn more about it!');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +74,7 @@ ALTER TABLE `demo`
 -- AUTO_INCREMENT for table `demo`
 --
 ALTER TABLE `demo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

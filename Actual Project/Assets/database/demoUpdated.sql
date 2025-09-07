b65df79dc40d49d9e3f2b170105f9363b6be31eb
raudhahh-jpg <raudhahhsulaiman@gmail.com>
2025-09-07 12:43:36 +0800

    Add files via upload

---
+++ Actual Project/Assets/database/demoUpdated.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 07, 2025 at 06:41 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hackathon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `demo`
+--
+
+CREATE TABLE `demo` (
+  `id` int(11) NOT NULL,
+  `line` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `demo`
+--
+
+INSERT INTO `demo` (`id`, `line`) VALUES
+(1, 'Hello, Welcome to Awang Budiman: Leap towards Goodness!'),
+(2, 'I am Awang Budiman, I will be your \'budi\' throughout this game'),
+(3, 'I\'ve lost my keris. I need your help to find it.'),
+(4, 'Use arrow and space to move'),
+(5, 'Click on the pause button to explore more'),
+(6, 'Now you are ready to help me find my keris!'),
+(7, 'Move to the right until the end of the screen'),
+(8, 'Welcome to the first level.'),
+(9, 'Hit the info icon to learn more about it!');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `demo`
+--
+ALTER TABLE `demo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `demo`
+--
+ALTER TABLE `demo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
